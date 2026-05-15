13c6dd5185050e12b37d9e01289dfe0102c582f8
John the Scott <jmscott@setspace.com>
2026-05-14 23:47:48 -0500

    stable checkpoint of merge-ParseRequestURI

--- merge-ParseRequestURI.go
+++ merge-ParseRequestURI.go
@@ -1,3 +1,14 @@
+/*
+ *  Synopsis:
+ *	Merge rows from stdin into various tables kamayama.ParseRequestURI*
+ *  Exit Code:
+ *	0	all rows merged.
+ *	1	parse error on a url read from stdin. does not stop parsing.
+ *	2	unexpeccted database error
+ *	3	unexpected system error
+ *  Note:
+ *	Oops.  rename to upsert-ParseRequestURI
+ */
 package main
 
 import (
@@ -7,23 +18,172 @@ import (
 	"github.com/jackc/pgx/v5"
 	"os"
 	"strings"
+	"net/url"
 )
+
+/*
+ *  0:	all urls parsed ok and merged into db (OK)
+ *  1:	at least one url parse failed, error merged into db (WARN)
+ *  2:	fatal sql error (ERROR)
+ *  3:	fatal unexpected system error (ERROR)
+ */
+var exit_code		int
+
+//  current line being scanned
+var scan_line_no	uint64
+
+//  current db connection.  pgx is single threaded.
+var db		*pgx.Conn
+
+//  background context required by pgx.
+//
+//  Note: can ctx be gotten from *db?
+var ctx			context.Context
 
 func die(format string, args ...interface{}) {
 	
 	fmt.Fprintf(os.Stderr, "ERROR: " + format + "\n", args...)
-	os.Exit(1)
+	os.Exit(3)
+}
+
+//  die() on sql error
+func dieq(format string, args ...interface{}) {
+	
+	format = fmt.Sprintf(
+			"ERROR: SQL(%s): %s",
+			os.Getenv("PGDATABASE"),
+			format,
+	)
+
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(2)
+}
+
+func warn(format string, args ...interface{}) {
+	
+	format = fmt.Sprintf("WARN: stdin line %d: %s\n", scan_line_no, format)
+	fmt.Fprintf(os.Stderr, format, args...)
+	exit_code = 1
+}
+
+func warn_url(rawURL string, parse_err error) {
+
+	warn("ParseRequestURI() failed: %s",  parse_err)
+	_, err := db.Exec(ctx, `
+			INSERT INTO golang_net_url_ParseRequestURI(
+				rawURL, error
+			) VALUES ($1, $2)
+			  ON CONFLICT
+			  	DO NOTHING
+			;`, rawURL, parse_err.Error(),
+	)
+	if err != nil {
+		dieq("db.Exec(error) failed: %s", err)
+	}
+}
+
+//  insert an error tuple for a parsed url
+func merge(rawURL string) {
+
+	txn, err := db.Begin(ctx)
+	if err != nil {
+		dieq("db.Begin(ctx) failed: %s", err)
+	}
+	defer txn.Rollback(ctx)		// becomes null-op after commit 
+
+	_, err = db.Exec(ctx, `SET search_path TO kamayama`)
+	if err != nil {
+		dieq("db.Exec(search_path) failed: %s", err)
+	}
+
+	//  parse url and issue warning if parse fails.
+	u, err := url.ParseRequestURI(rawURL)
+	if err != nil {
+		warn_url(rawURL, err)
+		return
+	}
+	userinfo := ""
+	username := ""
+
+	ui := u.User
+	if ui != nil {
+		userinfo = ui.String()
+		username = ui.Username()
+	}
+
+	//  sql INSERT parsed url and croak on any database error
+	_, err = db.Exec(ctx, `
+		INSERT INTO golang_net_url_ParseRequestURI(
+			rawURL,
+			Scheme, UserInfo, Username, Host, Hostname, Path,
+			Fragment, EscapedPath, EscapedFragment, RawQuery,
+			RawPath, RawFragment, ForceQuery, OmitHost, IsAbs,
+			Port
+		) VALUES (
+			$1,
+			$2, $3, $4, $5, $6, $7,
+			$8, $9, $10, $11,
+			$12, $13, $14, $15, $16,
+			$17
+		) ON CONFLICT
+			DO NOTHING
+		;`,
+		rawURL,
+		u.Scheme, userinfo, username, u.Host, u.Hostname(), u.Path,
+		u.Fragment, u.EscapedPath(), u.EscapedFragment(), u.RawQuery,
+		u.RawPath, u.RawFragment, u.ForceQuery, u.OmitHost, u.IsAbs(),
+		u.Port(),
+	)
+	if err != nil {
+		dieq("db.Exec(ParseRequestURI) failed: %s", err)
+	}
+
+	//  INSERT into golang_net_url_Query
+	q := u.Query()
+	for a, vals := range q {
+		_, err = db.Exec(ctx, `
+			INSERT INTO
+				golang_net_url_Query(rawURL, arg)
+				VALUES ($1, $2)
+			  ON CONFLICT
+			  	DO NOTHING
+			;
+		`, rawURL, a)
+		if err != nil {
+			dieq("db.Exec(Query) failed: %s", err)
+		}
+		for i, v := range vals {
+			_, err = db.Exec(
+				ctx,
+				`INSERT INTO
+					golang_net_url_Query_Values(
+						rawURL,
+						arg,
+						array_order,
+						value
+					) VALUES ($1, $2, $3, $4)
+				;`,
+				rawURL, a, i, v)
+			if err != nil {
+				dieq("db.Exec(QueryValues) failed: %s", err)
+			}
+		}
+	}
+	txn.Commit(ctx)
 }
 
 func main() {
+
+	var err error
 
 	argc := len(os.Args)
 	if argc != 1 {
 		die("wrong cli arg count: got %d, want 0", argc)
 	}
 
-	conn, err := pgx.Connect(
-			context.Background(),
+	ctx = context.Background()
+	db, err = pgx.Connect(
+			ctx,
 			fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 				os.Getenv("PGUSER"),
 				os.Getenv("PGPASSWORD"),
@@ -33,18 +193,17 @@ func main() {
 			),
 	)
 	if err != nil {
-		die(
+		dieq(
 			"pgx.Connect(%s) failed: %s",
 			os.Getenv("PGDATABASE"),
 			err,
 		)
 	}
-	defer conn.Close(context.Background()) // Close connection when done
+	defer db.Close(context.Background()) // Close connection when done
 
 	in := bufio.NewReader(os.Stdin)
-	line_no := 0
 	for {
-		line_no++
+		scan_line_no++
 		rawURL, err := in.ReadString('\n')
 		if err != nil {
 			if err.Error() == "EOF" {
@@ -54,10 +213,9 @@ func main() {
 		}
 		rawURL = strings.TrimRight(rawURL, "\n")
 		if rawURL == "" {
-			die("stdin: empty string near line %d", line_no)
+			die("stdin: empty string near line %d", scan_line_no)
 		}
-
+		merge(rawURL)
 	}
-
-	os.Exit(0)
+	os.Exit(exit_code)
 }
--- schema.sql
+++ schema.sql
@@ -14,43 +14,91 @@ SET search_path TO kamayama;
 
 --  output of golang function net/url.ParseRequestURI(rawURL string)
 
+DROP DOMAIN IF EXISTS kamatime CASCADE;
+CREATE DOMAIN kamatime AS timestamp CHECK (
+
+	-- birthday of kamayama
+	value >= '2026-05-14 02:14:15.406082-05'
+) NOT NULL DEFAULT now();
+
 DROP TABLE IF EXISTS golang_net_url_ParseRequestURI CASCADE;
 CREATE TABLE golang_net_url_ParseRequestURI
 (
 	rawURL		text PRIMARY KEY,
+
 	error		text,
 
-	Schema		text NOT NULL,
+	Scheme		text,
 
 	--  the String() value of UserInfo structure
 	UserInfo	text,
+	Username	text,
 
-	Host		text NOT NULL,
-	Hostname	text NOT NULL,
+	Host		text,
+	Hostname	text,
 
-	Path		text NOT NULL,
-	Fragment	text NOT NULL,
+	Path		text,
+	Fragment	text,
 
-	EscapedPath	text NOT NULL,
-	EscapedFragment	text NOT NULL,
+	EscapedPath	text,
+	EscapedFragment	text,
 
-	RawQuery	text NOT NULL,
-	RawPath		text NOT NULL,
-	RawFragment	text NOT NULL,
-	ForceQuery	bool NOT NULL,
-	OmitHost	bool NOT NULL,
+	RawQuery	text,
+	RawPath		text,
+	RawFragment	text,
+	ForceQuery	bool,
+	OmitHost	bool,
 
-	IsAbs		bool NOT NULL,
+	IsAbs		bool,
 
-	Port		text NOT NULL,
+	Port		text CHECK (
+				Port ~ '^[0-9]{1,5}$'
+				OR
+				Port = ''
+			),
 
-	insert_time	timestamptz
-				NOT NULL
-				DEFAULT now()
+	insert_time	kamatime,
+
+	/*
+	 *  Can have either an error and no other values
+	 *  OR
+	 *  No error and some values.
+	 */
+	--  insure we do not have both errors and parsed values.
+	CONSTRAINT error_nullness CHECK ((
+		--  error happened, so other fields must be null
+		error IS NOT NULL
+		AND
+		(
+			Scheme, UserInfo,
+			Host, Hostname,
+			Path, EscapedPath, EscapedFragment,
+			RawQuery, RawPath, RawFragment,
+			ForceQuery, OmitHost, IsAbs,
+			Port
+		) IS NULL
+	     ) OR (
+	     	--  no error so some of other fields must exist
+	     	error IS NULL
+		AND
+		(
+			Scheme, UserInfo,
+			Host, Hostname,
+			Path, EscapedPath, EscapedFragment,
+			RawQuery, RawPath, RawFragment,
+			ForceQuery, OmitHost, IsAbs,
+			Port
+		) IS NOT NULL
+	    )
+	)
 );
 CREATE INDEX idx_golang_net_url_ParseRequestURI_itime
   ON
   	golang_net_url_ParseRequestURI(insert_time)
+;
+COMMENT ON TABLE golang_net_url_ParseRequestURI
+  IS
+  	'Request URL Frisjed by golang fun net.url.ParseRequestURI()'
 ;
 
 DROP TABLE IF EXISTS golang_net_url_Query;
@@ -63,20 +111,24 @@ CREATE TABLE golang_net_url_Query
 
 	PRIMARY KEY	(rawURL, arg)
 );
+COMMENT ON TABLE golang_net_url_Query
+  IS
+  	'Query args for frisked request url'
+;
 
 DROP TABLE IF EXISTS golang_net_url_Query_Values CASCADE;
 CREATE TABLE golang_net_url_Query_Values
 (
 	rawURL		text,
 	arg		text,
-
-	array_order	smallint CHECK (
-				array_order >= 0
-			),
+	array_order	smallint,
 
 	value		text NOT NULL,
 
-	PRIMARY KEY	(rawURL, arg, array_order)
+	PRIMARY KEY	(rawURL, arg, array_order),
+
+	FOREIGN KEY (rawURL, arg)
+			REFERENCES golang_net_url_Query
 );
 
 COMMIT TRANSACTION;
